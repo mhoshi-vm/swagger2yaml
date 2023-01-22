46d07a2c27c3f51e7b827e9bf01ddb372dee09a7
supplychain <supplychain>
2023-01-22 14:24:26 +0000

    supplychain@cluster.local

--- src/main/java/org/openapitools/api/HelloApiController.java
+++ src/main/java/org/openapitools/api/HelloApiController.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-22T14:06:56.079391Z[Etc/UTC]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-22T14:24:24.232599Z[Etc/UTC]")
 @Controller
 @RequestMapping("${openapi.sample.base-path:}")
 public class HelloApiController implements HelloApi {
