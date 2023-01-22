619582d5cdd5e2188d0a290e0624bd0bc446cf3a
supplychain <supplychain>
2023-01-22 16:07:33 +0000

    supplychain@cluster.local

--- src/main/java/org/openapitools/api/HelloApi.java
+++ src/main/java/org/openapitools/api/HelloApi.java
@@ -31,7 +31,7 @@ import java.util.Map;
 import java.util.Optional;
 import javax.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-22T14:30:43.608239Z[Etc/UTC]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-22T16:07:32.069148Z[Etc/UTC]")
 @Validated
 @Tag(name = "hello", description = "the hello API")
 public interface HelloApi {
